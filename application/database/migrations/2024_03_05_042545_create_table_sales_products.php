72aefa6416cd42c6733a9d81eeea6f9d936e9c63
felipehenrique159 <felipecod6@hotmail.com>
2024-03-05 20:56:03 -0300

    fix: corrigido campo tabela sales_products

--- application/database/migrations/2024_03_05_042545_create_table_sales_products.php
+++ application/database/migrations/2024_03_05_042545_create_table_sales_products.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('sales_products', function (Blueprint $table) {
             $table->id();
             $table->integer('sales_id');
-            $table->integer('produto_id');
+            $table->integer('product_id');
         });
     }
 
